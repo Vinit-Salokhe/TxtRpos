943a4b2e3547fb6342fb598b754b0a801fc5173c
Vinit-Salokhe <vinitsalokhe3540@gmail.com>
2021-03-08 14:32:17 +0530

    My first commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+package com.demo;
+
+public class Main {
+	static int count = 0;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		if(count<3){
+		count++;
+		main(null);
+	}
+		else 
+		{return;}
+		
+	System.out.println("Hellow World");
+	}
+
+}
